13e4a17eff32e927451a22a921333e8d7034d04d
ahmadadptr001 <ahmadadptr@gmail.com>
2024-11-11 08:00:26 +0700

    2

--- src/App.js
+++ src/App.js
@@ -17,11 +17,7 @@ import Main from './components/main/main';
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
         <Route basename="/anime-project"
-=======
-        <Route
->>>>>>> 3582a9384c6248ca7c4e080b045ad2042f5de6dc
           path="/"
           element={
             <>
